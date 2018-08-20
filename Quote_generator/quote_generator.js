a39226e9da17dc562f07c845071e4cb1a0b55e9e
Verdanmaer <verneradam618@gmail.com>
2018-08-20 21:10:39 +0200

    format

--- Quote_generator/quote_generator.js
+++ Quote_generator/quote_generator.js
@@ -1,18 +1,15 @@
 function getQuote() {
-    $.ajax({
-      url: "https://api.forismatic.com/api/1.0/?",
-      dataType: "jsonp", 
-      data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
-      success: function(response){
-        $("#qte").html(response.quoteText);
-        $("#author").html("<br/>&dash; " + response.quoteAuthor + " &dash;");
-        $("#tweet").attr("href", "https://twitter.com/home/?status=" + response.quoteText +' (' + response.quoteAuthor + ')');
-      }
-    });
+  $.ajax({
+    url: "https://api.forismatic.com/api/1.0/?",
+    dataType: "jsonp", 
+    data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
+    success: function(response){
+      $("#qte").html(response.quoteText);
+      $("#author").html("<br/>&dash; " + response.quoteAuthor + " &dash;");
+      $("#tweet").attr("href", "https://twitter.com/home/?status=" + response.quoteText +' (' + response.quoteAuthor + ')');
+    }
+  });
 }
   
-$(function() {
-    getQuote();  
-});
-
+$(function() {getQuote();});
 $('#new-quote').on('click', getQuote);
