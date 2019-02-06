fba90e7f16a57867f5e7c3bde3b72f16af9db1b1
Adam Verner <verneradam618@gmail.com>
2019-02-06 01:25:51 +0100

    Revert "fixed pomodoro"
    This reverts commit 7b77c5f65e1ba988bf7561c31f75b53529a0b952.

--- projects/Pomodoro/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import App from "./components/App";
-
-import "./style/App.css";
-
-ReactDOM.render(<App />, document.querySelector("#root"));
